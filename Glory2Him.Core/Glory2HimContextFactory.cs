2acb17081345a21a5ebb8f8049cb9f6bd52b8b9e
Christo du Toit <christo@dutoit.co.uk>
2025-10-29 17:55:50 +0000

    BROKERS: Storage Broker

---
+++ Glory2Him.Core/Glory2HimContextFactory.cs
@@ -0,0 +1,38 @@
+﻿// ────────────────────────────────────────────────────────────────────────────────
+// Copyright (c) Glory 2 Him. All rights reserved.
+// Licensed under the Glory 2 Him Software License (G2HSL).
+// See License.txt in the project root for full license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 (NIV) "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15
+// ────────────────────────────────────────────────────────────────────────────────
+
+using System.Collections.Generic;
+using Glory2Him.Core.Brokers.Storages.Sql;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Glory2Him.Core
+{
+    internal class Glory2HimContextFactory : IDesignTimeDbContextFactory<StorageBroker>
+    {
+        public StorageBroker CreateDbContext(string[] args)
+        {
+            List<KeyValuePair<string, string?>> config = new List<KeyValuePair<string, string?>>
+            {
+                new KeyValuePair<string, string?>(
+                    key: "ConnectionStrings:Glory2HimConnectionString",
+
+                    value:
+                        "Server=(localdb)\\MSSQLLocalDB;Database=Glory2Him;" +
+                        "Trusted_Connection=True;MultipleActiveResultSets=true"),
+            };
+
+            var configurationBuilder = new ConfigurationBuilder()
+                .AddInMemoryCollection(initialData: config);
+
+            IConfiguration configuration = configurationBuilder.Build();
+            return new StorageBroker(configuration);
+        }
+    }
+}
