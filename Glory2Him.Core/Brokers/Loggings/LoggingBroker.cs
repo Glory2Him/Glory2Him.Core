ebe40981fb69f9217c74811277429a904bd18ca9
Christo du Toit <christo@dutoit.co.uk>
2025-11-20 20:58:36 +0000

    001 - BROKERS: Loggings - Log Information

---
+++ Glory2Him.Core/Brokers/Loggings/LoggingBroker.cs
@@ -0,0 +1,26 @@
+// ────────────────────────────────────────────────────────────────────────────────
+// Copyright (c) Glory 2 Him. All rights reserved.
+// Licensed under the Glory 2 Him Software License (G2HSL).
+// See License.txt in the project root for full license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 (NIV) "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15
+// ────────────────────────────────────────────────────────────────────────────────
+
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Glory2Him.Core.Brokers.Loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+
+        public async ValueTask LogInformationAsync(string message) =>
+            this.logger.LogInformation(message);
+    }
+}
