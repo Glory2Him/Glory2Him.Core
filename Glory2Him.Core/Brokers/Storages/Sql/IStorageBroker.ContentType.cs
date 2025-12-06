d257fa6c4b2e666ce5d8172314b70a3f7b23149d
Christo du Toit <christo@dutoit.co.uk>
2025-12-06 19:02:44 +0000

    BROKERS: Content Type

--- Glory2Him.Core/Brokers/Storages/Sql/IStorageBroker.ContentType.cs
+++ Glory2Him.Core/Brokers/Storages/Sql/IStorageBroker.ContentType.cs
@@ -7,8 +7,30 @@
 // https://mark.bible/mark-16-15
 // ────────────────────────────────────────────────────────────────────────────────
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Glory2Him.Core.Models.Foundations.ContentTypes;
+
 namespace Glory2Him.Core.Brokers.Storages.Sql
 {
     public partial interface IStorageBroker
-    { }
+    {
+        ValueTask<ContentType> InsertContentTypeAsync(
+        ContentType contentType);
+
+        ValueTask<IQueryable<ContentType>> SelectAllContentTypesAsync();
+        ValueTask<ContentType> SelectContentTypeByIdAsync(Guid contentTypeId);
+
+        ValueTask<ContentType> UpdateContentTypeAsync(
+            ContentType contentType);
+
+        ValueTask<ContentType> DeleteContentTypeAsync(
+            ContentType contentType);
+
+        ValueTask BulkInsertContentTypesAsync(List<ContentType> contentTypes);
+        ValueTask BulkUpdateContentTypesAsync(List<ContentType> contentTypes);
+        ValueTask BulkDeleteContentTypesAsync(List<ContentType> contentTypes);
+    }
 }
