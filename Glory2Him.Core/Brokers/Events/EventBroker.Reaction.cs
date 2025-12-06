04028ec7e16d6bcf9386f9661faf034e869682be
Christo du Toit <christo@dutoit.co.uk>
2025-12-06 23:31:32 +0000

    BROKERS: Reaction Event Broker

---
+++ Glory2Him.Core/Brokers/Events/EventBroker.Reaction.cs
@@ -0,0 +1,27 @@
+﻿// ────────────────────────────────────────────────────────────────────────────────
+// Copyright (c) Glory 2 Him. All rights reserved.
+// Licensed under the Glory 2 Him Software License (G2HSL).
+// See License.txt in the project root for full license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 (NIV) "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15
+// ────────────────────────────────────────────────────────────────────────────────
+
+using System;
+using System.Threading.Tasks;
+using Glory2Him.Core.Models.Foundations.Reactions;
+using LeVent.Clients;
+
+namespace Glory2Him.Core.Brokers.Events
+{
+    public partial class EventBroker
+    {
+        public ILeVentClient<Reaction> ReactionEvents { get; set; }
+
+        public ValueTask PublishReactionAsync(Reaction reaction, string eventName = null) =>
+            this.ReactionEvents.PublishEventAsync(reaction, eventName);
+
+        public void SubscribeToReactionEvent(Func<Reaction, ValueTask> reactionEventHandler, string eventName = null) =>
+            this.ReactionEvents.RegisterEventHandler(reactionEventHandler, eventName);
+    }
+}
---
+++ Glory2Him.Core/Brokers/Events/IEventBroker.Reaction.cs
@@ -0,0 +1,21 @@
+﻿// ────────────────────────────────────────────────────────────────────────────────
+// Copyright (c) Glory 2 Him. All rights reserved.
+// Licensed under the Glory 2 Him Software License (G2HSL).
+// See License.txt in the project root for full license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 (NIV) "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15
+// ────────────────────────────────────────────────────────────────────────────────
+
+using System;
+using System.Threading.Tasks;
+using Glory2Him.Core.Models.Foundations.Reactions;
+
+namespace Glory2Him.Core.Brokers.Events
+{
+    public partial interface IEventBroker
+    {
+        ValueTask PublishReactionAsync(Reaction reaction, string eventName = null);
+        void SubscribeToReactionEvent(Func<Reaction, ValueTask> reactionEventHandler, string eventName = null);
+    }
+}
