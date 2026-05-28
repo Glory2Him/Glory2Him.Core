21cacdf8ea5343b99f9fad795ace24811931ebaf
Christo du Toit <christo@dutoit.co.uk>
2026-05-28 19:58:44 +0100

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync -> FAIL

--- Glory2Him.Core.Tests.Unit/Services/Foundations/ContentItems/ContentItemServiceTests.RetrieveAll.Exceptions.cs
+++ Glory2Him.Core.Tests.Unit/Services/Foundations/ContentItems/ContentItemServiceTests.RetrieveAll.Exceptions.cs
@@ -14,6 +14,7 @@ using System.Threading.Tasks;
 using FluentAssertions;
 using Glory2Him.Core.Models.Foundations.ContentItems;
 using Glory2Him.Core.Models.Foundations.ContentItems.Exceptions;
+using Microsoft.Data.SqlClient;
 using Moq;
 
 namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentItems
@@ -81,6 +82,54 @@ namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentItems
             // then
             await Assert.ThrowsAsync<OperationCanceledException>(
                 retrieveAllContentItemsTask.AsTask);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            SqlException sqlException = GetSqlException();
+
+            var failedStorageContentItemException = new FailedStorageContentItemException(
+                message: "Failed content item storage error occurred, contact support.",
+                innerException: sqlException,
+                data: sqlException.Data);
+
+            var expectedContentItemDependencyException = new ContentItemDependencyException(
+                message: "Content item dependency error occurred, contact support.",
+                innerException: failedStorageContentItemException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllContentItemsAsync())
+                    .ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<IQueryable<ContentItem>> retrieveAllContentItemsTask =
+                this.contentItemService.RetrieveAllContentItemsAsync(
+                    TestContext.Current.CancellationToken);
+
+            ContentItemDependencyException actualContentItemDependencyException =
+                await Assert.ThrowsAsync<ContentItemDependencyException>(
+                    retrieveAllContentItemsTask.AsTask);
+
+            // then
+            actualContentItemDependencyException.Should().BeEquivalentTo(
+                expectedContentItemDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllContentItemsAsync(),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCriticalAsync(It.Is(
+                    SameExceptionAs(expectedContentItemDependencyException))),
+                Times.Once);
 
             this.securityAuditBrokerMock.VerifyNoOtherCalls();
             this.dateTimeBrokerMock.VerifyNoOtherCalls();
