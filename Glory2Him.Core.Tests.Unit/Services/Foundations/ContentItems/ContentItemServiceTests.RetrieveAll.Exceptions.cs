0fcfeff7ff2b5bb432993b87d296b4d2fc1815cc
Christo du Toit <christo@dutoit.co.uk>
2026-05-28 19:46:30 +0100

    ShouldThrowDependencyExceptionOnRetrieveAllIfOperationCanceledExceptionOccursAndLogItAsync -> FAIL

---
+++ Glory2Him.Core.Tests.Unit/Services/Foundations/ContentItems/ContentItemServiceTests.RetrieveAll.Exceptions.cs
@@ -0,0 +1,70 @@
+// ────────────────────────────────────────────────────────────────────────────────
+// Copyright (c) Glory 2 Him. All rights reserved.
+// Licensed under the Glory 2 Him Software License (G2HSL).
+// See License.txt in the project root for full license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 (NIV) "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15
+// ────────────────────────────────────────────────────────────────────────────────
+
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Glory2Him.Core.Models.Foundations.ContentItems;
+using Glory2Him.Core.Models.Foundations.ContentItems.Exceptions;
+using Moq;
+
+namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentItems
+{
+    public partial class ContentItemServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRetrieveAllIfOperationCanceledExceptionOccursAndLogItAsync()
+        {
+            // given
+            var operationCanceledException = new OperationCanceledException();
+
+            var timeoutContentItemException = new TimeoutContentItemException(
+                message: "Content item timed out, contact support.",
+                innerException: new TimeoutException(),
+                data: operationCanceledException.Data);
+
+            var expectedContentItemDependencyException = new ContentItemDependencyException(
+                message: "Content item dependency error occurred, contact support.",
+                innerException: timeoutContentItemException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllContentItemsAsync())
+                    .ThrowsAsync(operationCanceledException);
+
+            // when
+            ValueTask<IQueryable<ContentItem>> retrieveAllContentItemsTask =
+                this.contentItemService.RetrieveAllContentItemsAsync(
+                    TestContext.Current.CancellationToken);
+
+            ContentItemDependencyException actualContentItemDependencyException =
+                await Assert.ThrowsAsync<ContentItemDependencyException>(
+                    retrieveAllContentItemsTask.AsTask);
+
+            // then
+            actualContentItemDependencyException.Should().BeEquivalentTo(
+                expectedContentItemDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllContentItemsAsync(),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedContentItemDependencyException))),
+                Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
