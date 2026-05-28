c51ae6004ddfca582b33ad7e98eb846bf7c92afa
Christo du Toit <christo@dutoit.co.uk>
2026-05-28 22:11:11 +0100

    ShouldThrowDependencyValidationExceptionOnModifyIfDbUpdateConcurrencyExceptionOccursAndLogItAsync -> FAIL

--- Glory2Him.Core.Tests.Unit/Services/Foundations/ContentItems/ContentItemServiceTests.Modify.Exceptions.cs
+++ Glory2Him.Core.Tests.Unit/Services/Foundations/ContentItems/ContentItemServiceTests.Modify.Exceptions.cs
@@ -14,6 +14,7 @@ using FluentAssertions;
 using Glory2Him.Core.Models.Foundations.ContentItems;
 using Glory2Him.Core.Models.Foundations.ContentItems.Exceptions;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
 using Moq;
 
 namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentItems
@@ -135,6 +136,56 @@ namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentItems
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(
                     SameExceptionAs(expectedContentItemDependencyException))),
+                Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowDependencyValidationExceptionOnModifyIfDbUpdateConcurrencyExceptionOccursAndLogItAsync()
+        {
+            // given
+            ContentItem someContentItem = CreateRandomContentItem();
+            var dbUpdateConcurrencyException = new DbUpdateConcurrencyException();
+
+            var lockedContentItemException = new LockedContentItemException(
+                message: "Locked content item record, please try again later.",
+                innerException: dbUpdateConcurrencyException,
+                data: dbUpdateConcurrencyException.Data);
+
+            var expectedContentItemDependencyValidationException = new ContentItemDependencyValidationException(
+                message: "Content item dependency validation error occurred, fix the errors and try again.",
+                innerException: lockedContentItemException);
+
+            this.securityAuditBrokerMock.Setup(broker =>
+                broker.ApplyModifyAuditValuesAsync(someContentItem))
+                    .ThrowsAsync(dbUpdateConcurrencyException);
+
+            // when
+            ValueTask<ContentItem> modifyContentItemTask =
+                this.contentItemService.ModifyContentItemAsync(
+                    someContentItem,
+                    TestContext.Current.CancellationToken);
+
+            ContentItemDependencyValidationException actualContentItemDependencyValidationException =
+                await Assert.ThrowsAsync<ContentItemDependencyValidationException>(
+                    modifyContentItemTask.AsTask);
+
+            // then
+            actualContentItemDependencyValidationException.Should().BeEquivalentTo(
+                expectedContentItemDependencyValidationException);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.ApplyModifyAuditValuesAsync(someContentItem),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedContentItemDependencyValidationException))),
                 Times.Once);
 
             this.securityAuditBrokerMock.VerifyNoOtherCalls();
