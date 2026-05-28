cbbad212713c5da3755357c8b0a49259794da555
Christo du Toit <christo@dutoit.co.uk>
2026-05-28 20:04:44 +0100

    ShouldRetrieveContentItemByIdAsync -> FAIL

---
+++ Glory2Him.Core.Tests.Unit/Services/Foundations/ContentItems/ContentItemServiceTests.RetrieveById.Logic.cs
@@ -0,0 +1,55 @@
+// ────────────────────────────────────────────────────────────────────────────────
+// Copyright (c) Glory 2 Him. All rights reserved.
+// Licensed under the Glory 2 Him Software License (G2HSL).
+// See License.txt in the project root for full license information.
+// FREE TO USE TO HELP SHARE THE GOSPEL
+// Mark 16:15 (NIV) "Go into all the world and preach the gospel to all creation."
+// https://mark.bible/mark-16-15
+// ────────────────────────────────────────────────────────────────────────────────
+
+using System.Threading.Tasks;
+using FluentAssertions;
+using Glory2Him.Core.Models.Foundations.ContentItems;
+using Moq;
+
+namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentItems
+{
+    public partial class ContentItemServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveContentItemByIdAsync()
+        {
+            // given
+            ContentItem randomContentItem = CreateRandomContentItem();
+            ContentItem storageContentItem = randomContentItem;
+            ContentItem expectedContentItem = storageContentItem;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectContentItemByIdAsync(
+                    randomContentItem.Id,
+                    TestContext.Current.CancellationToken))
+                        .ReturnsAsync(storageContentItem);
+
+            // when
+            ContentItem actualContentItem =
+                await this.contentItemService.RetrieveContentItemByIdAsync(
+                    randomContentItem.Id,
+                    TestContext.Current.CancellationToken);
+
+            // then
+            actualContentItem.Should().BeEquivalentTo(expectedContentItem);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectContentItemByIdAsync(
+                    randomContentItem.Id,
+                    TestContext.Current.CancellationToken),
+                Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
