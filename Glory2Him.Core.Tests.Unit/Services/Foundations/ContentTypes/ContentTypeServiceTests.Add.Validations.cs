b960fa1abd3764dc68d2f4ceaa83ffa521d2ab01
Christo du Toit <christo@dutoit.co.uk>
2026-05-30 03:29:06 +0100

    ShouldThrowValidationExceptionOnAddIfUpdatedWhenIsNotSameAsCreatedWhenAndLogItAsync -> FAIL

--- Glory2Him.Core.Tests.Unit/Services/Foundations/ContentTypes/ContentTypeServiceTests.Add.Validations.cs
+++ Glory2Him.Core.Tests.Unit/Services/Foundations/ContentTypes/ContentTypeServiceTests.Add.Validations.cs
@@ -251,5 +251,78 @@ namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentTypes
             this.eventBrokerMock.VerifyNoOtherCalls();
             this.loggingBrokerMock.VerifyNoOtherCalls();
         }
+
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfUpdatedWhenIsNotSameAsCreatedWhenAndLogItAsync()
+        {
+            // given
+            string randomUserId = GetRandomString();
+            DateTimeOffset randomDateTimeOffset = GetRandomDateTimeOffset();
+            ContentType randomContentType = CreateContentTypeFiller(randomDateTimeOffset, randomUserId).Create();
+            ContentType invalidContentType = randomContentType;
+            invalidContentType.UpdatedWhen = GetRandomDateTimeOffset();
+
+            var invalidContentTypeException =
+                new InvalidContentTypeException(
+                    message: "Content type is invalid, fix the errors and try again.");
+
+            invalidContentTypeException.AddData(
+                key: nameof(ContentType.UpdatedWhen),
+                values: $"Date is not the same as {nameof(ContentType.CreatedWhen)}");
+
+            var expectedContentTypeValidationException =
+                new ContentTypeValidationException(
+                    message: "Content type validation error occurred, fix the errors and try again.",
+                    innerException: invalidContentTypeException);
+
+            this.securityAuditBrokerMock.Setup(broker =>
+                broker.ApplyAddAuditValuesAsync(invalidContentType))
+                    .ReturnsAsync(invalidContentType);
+
+            this.securityAuditBrokerMock.Setup(broker =>
+                broker.GetUserIdAsync())
+                    .ReturnsAsync(randomUserId);
+
+            this.dateTimeBrokerMock.Setup(broker =>
+                broker.GetCurrentDateTimeOffsetAsync())
+                    .ReturnsAsync(randomDateTimeOffset);
+
+            // when
+            ValueTask<ContentType> addContentTypeTask =
+                this.contentTypeService.AddContentTypeAsync(
+                    invalidContentType,
+                    TestContext.Current.CancellationToken);
+
+            ContentTypeValidationException actualContentTypeValidationException =
+                await Assert.ThrowsAsync<ContentTypeValidationException>(
+                    addContentTypeTask.AsTask);
+
+            // then
+            actualContentTypeValidationException.Should().BeEquivalentTo(
+                expectedContentTypeValidationException);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.ApplyAddAuditValuesAsync(invalidContentType),
+                Times.Once);
+
+            this.securityAuditBrokerMock.Verify(broker =>
+                broker.GetUserIdAsync(),
+                Times.Once);
+
+            this.dateTimeBrokerMock.Verify(broker =>
+                broker.GetCurrentDateTimeOffsetAsync(),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedContentTypeValidationException))),
+                Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
     }
 }
