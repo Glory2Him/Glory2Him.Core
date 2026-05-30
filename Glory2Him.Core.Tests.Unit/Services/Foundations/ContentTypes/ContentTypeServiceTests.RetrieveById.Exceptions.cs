9a51226df9090aa9c689d0342c2b842c9bea7517
Christo du Toit <christo@dutoit.co.uk>
2026-05-30 05:33:47 +0100

    ShouldThrowOperationCanceledExceptionOnRetrieveByIdIfCancellationRequestedAndLogItAsync -> PASS; ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync -> PASS

--- Glory2Him.Core.Tests.Unit/Services/Foundations/ContentTypes/ContentTypeServiceTests.RetrieveById.Exceptions.cs
+++ Glory2Him.Core.Tests.Unit/Services/Foundations/ContentTypes/ContentTypeServiceTests.RetrieveById.Exceptions.cs
@@ -11,6 +11,7 @@
 // ────────────────────────────────────────────────────────────────────────────────
 
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
 using Glory2Him.Core.Models.Foundations.ContentTypes;
@@ -121,6 +122,84 @@ namespace Glory2Him.Core.Tests.Unit.Services.Foundations.ContentTypes
             this.loggingBrokerMock.Verify(broker =>
                 broker.LogCriticalAsync(It.Is(
                     SameExceptionAs(expectedContentTypeDependencyException))),
+                Times.Once);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowOperationCanceledExceptionOnRetrieveByIdIfCancellationRequestedAndLogItAsync()
+        {
+            // given
+            Guid someContentTypeId = Guid.NewGuid();
+            var cancellationToken = new CancellationToken(canceled: true);
+
+            // when
+            ValueTask<ContentType> retrieveContentTypeByIdTask =
+                this.contentTypeService.RetrieveContentTypeByIdAsync(
+                    someContentTypeId,
+                    cancellationToken);
+
+            // then
+            await Assert.ThrowsAsync<OperationCanceledException>(
+                retrieveContentTypeByIdTask.AsTask);
+
+            this.securityAuditBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.eventBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+        [Fact]
+        public async Task ShouldThrowServiceExceptionOnRetrieveByIdIfServiceErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someContentTypeId = Guid.NewGuid();
+            var serviceException = new Exception();
+
+            var failedContentTypeServiceException = new FailedContentTypeServiceException(
+                message: "Failed content type service error occurred, please contact support.",
+                innerException: serviceException,
+                data: serviceException.Data);
+
+            var expectedContentTypeServiceException = new ContentTypeServiceException(
+                message: "Content type service error occurred, contact support.",
+                innerException: failedContentTypeServiceException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectContentTypeByIdAsync(
+                    someContentTypeId,
+                    TestContext.Current.CancellationToken))
+                        .ThrowsAsync(serviceException);
+
+            // when
+            ValueTask<ContentType> retrieveContentTypeByIdTask =
+                this.contentTypeService.RetrieveContentTypeByIdAsync(
+                    someContentTypeId,
+                    TestContext.Current.CancellationToken);
+
+            ContentTypeServiceException actualContentTypeServiceException =
+                await Assert.ThrowsAsync<ContentTypeServiceException>(
+                    retrieveContentTypeByIdTask.AsTask);
+
+            // then
+            actualContentTypeServiceException.Should().BeEquivalentTo(
+                expectedContentTypeServiceException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectContentTypeByIdAsync(
+                    someContentTypeId,
+                    TestContext.Current.CancellationToken),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogErrorAsync(It.Is(
+                    SameExceptionAs(expectedContentTypeServiceException))),
                 Times.Once);
 
             this.securityAuditBrokerMock.VerifyNoOtherCalls();
