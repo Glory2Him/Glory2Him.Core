166b53c273afe6a28b147b6650245117f52681aa
Christo du Toit <christo@dutoit.co.uk>
2026-08-11 15:04:23 +0100

    DATA: Configure The BlockOnZeroApprovalScore Default

---
+++ Glory2Him.Core/Migrations/20260811140336_ConfigureBlockOnZeroApprovalScoreDefault.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Glory2Him.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class ConfigureBlockOnZeroApprovalScoreDefault : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "BlockOnZeroApprovalScore",
+                table: "ApprovalSettings",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "BlockOnZeroApprovalScore",
+                table: "ApprovalSettings",
+                type: "bit",
+                nullable: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldDefaultValue: false);
+        }
+    }
+}
